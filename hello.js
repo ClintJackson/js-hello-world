cf3e0b010483e500ba89c7fdb31d413a88474014
Clint Jackson <clint.jackson09@gmail.com>
2023-12-20 07:02:25 -0600

    Now says 'Hello, World!' Includes messing around with the code

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello, World!");
 
 /*  
 Next, PLAY! 
@@ -20,3 +20,22 @@ Modify your code on line 7 to try to accomplish the tasks listed below:
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
    9. Other. You choose!
 */
+console.log ('Hello');
+console.log (Math.sqrt(9) + 7);
+
+console.log ("This" + " " + "is" + " " + "a" + " " + "test")
+/* cool
+apparently the semicolon isn't necessary? */
+
+console.log ("Math.sqrt(9)" + "7");
+console.log ("Math.sqrt(9)" + "7")
+console.log ('Math.sqrt(9)' + '7');
+console.log (Math.sqrt(9) + '7');
+/* 
+console.log ("one" + " " + 1) console.log ("two" + " " + 2)
+it doesn't like that one lol*/
+console.log ("one" + " " + 1); console.log ("two" + " " + 2) 
+/* OH the semicolon does do something!...I'll use them from now on, just in case.*/
+
+console.log ("\"The abuse of greatness is when it disjoins remorse from power\"" +
+"\n-William Shakespeare, in the play \"Julius Caesar\"");
